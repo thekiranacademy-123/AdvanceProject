18cb9935a12954793879156b43062b7b93b9aac0
thekiranacademy-123 <iranna@thekiranacademy.com>
2025-09-19 18:22:06 +0530

    Update LoginFinctionality.java

--- src/test/java/com/testcases/LoginFinctionality.java
+++ src/test/java/com/testcases/LoginFinctionality.java
@@ -4,5 +4,6 @@ public class LoginFinctionality {
 
 	public static void main(String[] args) {
 		System.out.println("i developed login functionality ");
+		System.out.println("get the updated code only !!!")
 	}
 }
